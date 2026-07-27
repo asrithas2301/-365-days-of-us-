97ba87c5a948bfd2df6e0e2af49b4130d52bb280
Asritha <asrithas2301@gmail.com>
2026-07-27 12:10:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector("button");
+
+    button.addEventListener("click", () => {
+        alert("Happy 1st Anniversary ❤️\nOur journey has just begun! 💖");
+    });
+});
